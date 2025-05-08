30a953e8cc56ef06e9d186792b52915ef67706f2
RobertTSL <robertstewartteazelegleu@gmail.com>
2025-05-08 14:52:31 -0700

    Update talleres.py

--- trabajoFinal/talleres.py
+++ trabajoFinal/talleres.py
@@ -1,6 +1,17 @@
 import datos
 def agregarTalleres():
   idtaller = int(input("ID del Taller: "))
-  for id in 
+  for id in datos.talleres:
+    if idtaller == id:
+      print("Ya hay un taller con ese ID \npor favor seleccione otro número\n ")
+      agregarTalleres()
+  nombreTaller = input("Nombre del Taller: ")
+  fechaInicio = input ("Fecha de inicio del taller: ")
+  turno = input ("Ingrese el turno AM/PM").upper()
+  horaInicio = input ("Hora inicio del taller: ")
+  duracionTaller = input ("Duración del taller en horas: ")
+  lugarTaller = input ("Aula del taller: ").upper()
+  capEstudiantes = int(input("Cupo maximo de estudiantes: "))
+  costoTaller = float(input("Costo del taller: "))
   
   
