b7ebebe59c725ce44d592f3f355cedfe91d361fb
Luis Isiordia <121597281+LuisIsiordia@users.noreply.github.com>
2025-05-08 13:54:29 -0700

    Update datos.py

--- trabajoFinal/datos.py
+++ trabajoFinal/datos.py
@@ -1,1 +1,39 @@
+estudiantes = [
 
+    (65676, "Juan Castelo Ruiz", "juan.castelo@gmail.com", "6441232425", True),
+
+    (65687, "Julio López Estrada", "julio.lop@gmail.com", "6441750026", False),
+
+    (65693, "Jhoana Madrigal Castro", "j.mcastro@gmail.com", "6441762499", False),
+
+    (65699, "Michelle Perez Ion", "mperezi18@gmail.com", "6441782400", False),
+
+    (65703, "Andres Pacheco Solis", "pacheco7659@gmail.com", "6441982411", False),
+
+    (65733, "Antonio García Tineo", "antoniogtineo@gmail.com", "6441652422", False),
+
+    (65754, "Perla Esquer Vega", "perlita19obr@gmail.com", "6441882433", True),
+
+    (65798, "Maritza Vega Ruiz", "mvegaruiz@gmail.com", "6441006515", True),
+
+    (65856, "Héctor Figueroa Ozuna", "hfigue22@gmail.com", "6441552477", False),
+
+    (65887, "Francisco Sauceda Vega", "fco.sauceda@hotmail.com", "6441332474", False),
+
+    (65895, "Patricia Estrella Sonora", "paty19son@gmail.com", "6441008899", False),
+
+    (65907, "Juana Escalante Ian", "juanita43@gmail.com", "6441006532", False),
+
+    (65932, "Kevin Sotelo Duarte", "kevinsotelo@gmail.com", "6441875498", True),
+
+    (65996, "Carlos Zamudio Soto", "zamudioobr@gmail.com", "6441234565", False),
+
+    (65998, "Yolanda Valdez Osuna", "yolyvaldez18@gmail.com", "6441985410", False),
+
+    (66006, "Amalia Miranda Soqui", "amaliamir2005@gmail.com", "6441094387", True),
+
+    (66178, "Pedro Hernandez Silva", "pedrosilva2@gmail.com", "6441121233", False),
+
+    (66197, "Catalina Mungaro Velez", "caty.m.v@gmail.com", "6441767676", False),
+
+]
