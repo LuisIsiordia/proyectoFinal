876333773de61ca8e28183563e22d7024eb2c319
RobertTSL <robertstewartteazelegleu@gmail.com>
2025-05-11 00:28:16 -0700

    Update talleres.py

--- trabajoFinal/talleres.py
+++ trabajoFinal/talleres.py
@@ -2,12 +2,18 @@ import datos
 import validar
 def agregarTalleres():
   import menu
-  idtaller = int(input("ID del Taller: "))
+  while True:
+        try:
+            idtaller = int(input("ID del Taller: "))
+            break
+        except ValueError:
+            print("Tipo de dato incorrecto, ingrese un número")
+            continue
   for id in datos.talleres:
     if idtaller == id:
       print("Ya hay un taller con ese ID \npor favor seleccione otro número\n ")
       agregarTalleres()
-    break
+      break
   nombreTaller = input("Nombre del Taller: ")
   fechaInicio = input ("Fecha de inicio del taller: ")
   turno = input ("Ingrese el turno AM/PM: ").upper()
@@ -44,9 +50,13 @@ def agregarTalleres():
     print(f" Lugar: {dato['lugar']}")
     print(f" Capacidad: {dato['capacidad']} estudiantes")
     print(f" Costo: ${dato['costo']:.2f}")
-  respuestaAñadir = input("¿Quiere añadir otro taller?").upper()
+  respuestaAñadir = input("¿Quiere añadir otro taller?: ").upper()
   validar.validarAñadir(respuestaAñadir)
   if respuestaAñadir == "S":
     agregarTalleres()
   elif respuestaAñadir == "N":
     menu.menu()
+
+
+  
+  
