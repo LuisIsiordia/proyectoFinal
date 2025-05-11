c7ff38dfaa5e2f3fc552642d607910eebaada1db
RobertTSL <robertstewartteazelegleu@gmail.com>
2025-05-10 23:35:04 -0700

    Update menu.py

--- trabajoFinal/menu.py
+++ trabajoFinal/menu.py
@@ -1,31 +1,32 @@
 import talleres
+def menu():
+    print("\n 1.Agregar un nuevo taller al catalogo\n",
+        "2.Dar de baja un taller del catalogo\n",
+        "3.Actualizar los datos de un taller\n",
+        "4.Mostrar el listado de talleres en el catalogo\n",
+        "5.Registrar una inscripción por folio\n",
+        "6.Cancelar una inscripción por folio\n",
+        "7.Mostrar el listado de inscripciones por taller\n",
+        "8.Mostrar los estudiantes vigentes\n",
+        "9.Terminar operacion\n")
+    resp=input("Teclee su opción: ")
 
-print("1.Agregar un nuevo taller al catalogo\n",
-      "2.Dar de baja un taller del catalogo\n",
-      "3.Actualizar los datos de un taller\n",
-      "4.Mostrar el listado de talleres en el catalogo\n",
-      "5.Registrar una inscripción por folio\n",
-      "6.Cancelar una inscripción por folio\n",
-      "7.Mostrar el listado de inscripciones por taller\n",
-      "8.Mostrar los estudiantes vigentes\n",
-      "9.Terminar operacion\n")
-resp=input("Teclee su opción: ")
-
-if resp=='1':
-    talleres.agregarTalleres
-elif resp=='2':
-    print("2")
-elif resp=='3':        
-    print("3")
-elif resp=='4':
-    print("4")
-elif resp=='5':
-    print("5")
-elif resp=='6':
-    print("6")
-elif resp=='7':
-    print("7")
-elif resp=='8':
-    print("8")
-elif resp=='9':
-    print("fin")
+    if resp=='1':
+        talleres.agregarTalleres()
+    elif resp=='2':
+        print("2")
+    elif resp=='3':        
+        print("3")
+    elif resp=='4':
+        print("4")
+    elif resp=='5':
+        print("5")
+    elif resp=='6':
+        print("6")
+    elif resp=='7':
+        print("7")
+    elif resp=='8':
+        print("8")
+    elif resp=='9':
+        print("fin")
+menu()
